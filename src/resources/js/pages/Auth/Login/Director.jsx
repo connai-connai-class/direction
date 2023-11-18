d22ff7b3bd5a01e74b408e663d10c5cd3e00b3f4
maccha <chihokoku8060@gmail.com>
2023-11-18 13:09:35 +0900

    add: AuthController for creator

---
+++ src/resources/js/pages/Auth/Login/Director.jsx
@@ -0,0 +1,31 @@
+import { Head } from "@inertiajs/react";
+import { Typography, Divider } from "@mui/material";
+import { Link } from "@ui-elements";
+import { Login, SocialLogin } from "@features";
+import { AuthLayout } from "@layouts";
+
+export default function LoginPage({ appName = "", ...props }) {
+  return (
+    <>
+      <Head title="Sign in" />
+      <AuthLayout>
+        <Typography variant="h4">Sign in to {appName}</Typography>
+
+        <Typography variant="body2" sx={{ mt: 2, mb: 5 }}>
+          Don’t have an account?
+          <Link className="ml-1" href={route("register")}>
+            Get started
+          </Link>
+        </Typography>
+        <SocialLogin />
+
+        <Divider sx={{ my: 3 }}>
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            OR
+          </Typography>
+        </Divider>
+        <Login {...props} />
+      </AuthLayout>
+    </>
+  );
+}
