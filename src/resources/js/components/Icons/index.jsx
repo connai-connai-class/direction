7d514f17d90a1ad557d7c7fe3c3e0ef319051d50
connaiconnai <connaiconnai5252@gmail.com>
2023-11-25 05:21:48 +0900

    add: sidenav

--- src/resources/js/components/Icons/index.jsx
+++ src/resources/js/components/Icons/index.jsx
@@ -1,2 +1,7 @@
 import DropdownIcon from "./svg/DownArrow";
 export { DropdownIcon };
+
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+export const HomeIcon = (props) => {
+  return <HomeOutlinedIcon {...props} />;
+};
