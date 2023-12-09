939553e603f26d8318598469aae161082e5ae6ac
maccha <chihokoku8060@gmail.com>
2023-12-09 12:37:45 +0900

    fix:integer column

--- src/database/migrations/2023_12_08_043520_create_directions_table.php
+++ src/database/migrations/2023_12_08_043520_create_directions_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
     Schema::create('directions', function (Blueprint $table) {
       $table->id();
       $table->string('direction_code', 3)->comment('指示コード');
-      $table->string('diretion_name', 50)->comment('指示名');
+      $table->string('direction_name', 50)->comment('指示名');
       $table->text('template_name')->comment('テンプレート文章');
-      $table->integer('order_no', 1)->comment('並び順');
+      $table->integer('order_no')->comment('並び順');
       $table->timestamps();
       $table->softDeletes();
     });
